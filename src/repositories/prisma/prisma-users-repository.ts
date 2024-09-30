b4c387059495e8b541f3eaa88e115b2067bdf009
Cesar Emmanuel <cesaremmmanul@gmail.com>
2024-09-30 19:45:25 -0300

    feat: implements check ins prisma repository

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -3,15 +3,18 @@ import type { Prisma } from '@prisma/client'
 import type { IUsersRepository } from '../users-repository'
 
 export class PrismaUsersRepository implements IUsersRepository {
+  async findById(id: string) {
+    const result = await prisma.user.findUnique({ where: { id } })
+    return result
+  }
+
   async findByEmail(email: string) {
     const result = await prisma.user.findUnique({ where: { email } })
-
     return result
   }
 
   async create(data: Prisma.UserCreateInput) {
     const result = await prisma.user.create({ data })
-
     return result
   }
 }
