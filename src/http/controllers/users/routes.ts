ebd92ab33eb1efdb814b1bfefe84e1ffe11bd425
Cesar Emmanuel <cesaremmmanul@gmail.com>
2024-10-05 16:16:48 -0300

    test: refresh token

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,10 +3,12 @@ import { registerUser } from './register-user'
 import { authenticate } from './authenticate'
 import { profile } from './profile'
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
+import { refreshToken } from './refresh'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', registerUser)
   app.post('/sessions', authenticate)
+  app.patch('/token/refresh', refreshToken)
 
   // Authenticated
   app.get('/me', { onRequest: [verifyJwt] }, profile)
